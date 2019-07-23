4fe06bf1b63d9904cae2ffbfdc60e240d3a55e74
darkTower@cat.pdx.edu <tet2@pdx.edu>
2019-07-23 00:06:12 -0700

    cleaned up main.rs

--- inventory/src/main.rs
+++ inventory/src/main.rs
@@ -1,10 +1,25 @@
 use std::io::Read;
 extern crate rustc_serialize;
-use rustc_serialize::json::Json;
 extern crate serde;
-extern crate serde_json;
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct User {
+  iconPath: String,
+  membershipType: u32,
+  membershipId: String,
+  displayName: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct UserResponse {
+  Response: User,
+  ErrorCode: u32,
+  ThrottleSeconds: u32,
+  ErrorStatus: String,
+  Message: String,
+  MessageData: Vec<String>,
+}
 
 fn get_api_key() -> String {
 	let mut f = std::fs::File::open("api-key.txt").expect("could not open api key");
@@ -13,84 +28,47 @@ fn get_api_key() -> String {
 	key.trim().to_string()
 }
 
-#[derive(Serialize, Deserialize)]
-struct Data {
-Responce: Responce,
-		  ErrorCode: usize,
-		  ThrottleSeconds: usize,
-		  ErrorStatus: String,
-		  Message: String,
-		  MessageData: String
-}
-
-#[derive(Serialize, Deserialize)]
-struct Responce { 
-		  iconPath: String,
-		  membershipType: String,
-		  membershipId: String,
-		  displayName: String,
-}
-
 fn main() {
 	let api_key = get_api_key();
 	//100 is profiles, 200 is characters, 201 is non-equiped items, 205 currently equiped items.
 	let request_type = String::from("205");
 	let platform = '2';
 	let player_name = String::from("cortical_iv");
-	let mut url = String::from("https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/");
+	let base_url = String::from("https://www.bungie.net/Platform/Destiny2/");
 
-
-	url.push(platform);
-	url.push('/');
-	url.push_str(&player_name);
-	url.push('/');
-
+	let mut search_url = base_url.clone();
+	search_url.push_str("SearchDestinyPlayer/");
+	search_url.push(platform);
+	search_url.push('/');
+	search_url.push_str(&player_name);
+	search_url.push('/');
 
 	let mut response = reqwest::Client::new()
-		.get(&url)
+		.get(&search_url)
 		.header("X-API-KEY", api_key)
 		.send()
 		.expect("Failed to send request");
 	let mut buf = String::new();
-	/*example responce {
-			"Response":[
-				{
-					"iconPath":"/img/theme/destiny/icons/icon_psn.png",
-					"membershipType":2,
-					"membershipId":"4611686018459314819",
-					"displayName":"cortical_iv"
-				}
-			],
-			"ErrorCode":1,
-			"ThrottleSeconds":0,
-			"ErrorStatus":"Success",
-			"Message":"Ok",
-			"MessageData":{}
-		}
-*/
 	response.read_to_string(&mut buf).expect("Failed to read response");
-	//let info: Data = serde_json::from_str(&buf);
-	//println!("{}",info["Message"]);
-	
-	let membershipId = String::from("4611686018459314819");
-
-	let equipment = get_gear(membershipId);
+	let user = get_json(buf);
+	let u: User = serde_json::from_str(&user).expect("failed to parse response");
+	let api_key_2 = get_api_key();//TODO figure out how to fix this!!
+	let equipment = get_gear(&api_key_2, base_url, platform, u.membershipId, request_type);
+	println!("{}",equipment)
 }
 
-fn get_gear(membershipId:String) -> String {
-let api_key = get_api_key();
+fn get_gear(api_key:&String, base_url:String, platform: char, membershipId:String, request_type:String) -> String {
+	let api_key = get_api_key();
+	let mut url = base_url;
 	//100 is profiles, 200 is characters, 201 is non-equiped items, 205 currently equiped items.
 	let request_type = String::from("205");
-	let platform = '2';
-	let player_name = String::from("cortical_iv");
-	let mut url = String::from("https://www.bungie.net/Platform/Destiny2/2/Profile/4611686018459314819/?components=205");
 
-/*
 	url.push(platform);
-	url.push('/');
-	url.push_str(&player_name);
-	url.push('/');
-*/
+	url.push_str("/Profile/");
+	url.push_str(&membershipId);
+	url.push_str("/?components=");
+	url.push_str(&request_type);
+	//let mut url = String::from("https://www.bungie.net/Platform/Destiny2/2/Profile/4611686018459314819/?components=205");
 
 	let mut response = reqwest::Client::new()
 		.get(&url)
@@ -98,7 +76,14 @@ let api_key = get_api_key();
 		.send()
 		.expect("Failed to send request");
 	let mut buf = String::new();
-response.read_to_string(&mut buf).expect("Failed to read response");
-println!("{}", buf);
-buf
+	response.read_to_string(&mut buf).expect("Failed to read response");
+	//println!("{}", buf);
+	buf
+}
+
+fn get_json(source:String) -> String {
+  let start_index = source.find("[").expect("failed to find open");
+  let end_index = source.find("]").expect("failed to find close");
+  let mut result = &source[(start_index + 1)..end_index];
+  result.to_string()
 }
