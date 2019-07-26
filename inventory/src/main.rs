31b006743348cc4e22eeefeb321e7f12e4a0523f
darkTower@cat.pdx.edu <tet2@pdx.edu>
2019-07-25 19:15:53 -0700

    item database connection

--- inventory/src/main.rs
+++ inventory/src/main.rs
@@ -1,23 +1,22 @@
 use std::io::Read;
-//extern crate rustc_serialize;
 
+extern crate rusqlite;
+use rusqlite::types::ToSql;
+use rusqlite::{Connection, Result as Output};
+use rusqlite::NO_PARAMS;
+use std::collections::HashMap;
 use rustc_serialize::json::Json;
 
 extern crate serde;
 use serde::{Deserialize, Serialize};
 use serde_json::{Deserializer, Value};
 use serde_json::Result;
-/*
-#[derive(Serialize, Deserialize)]
-struct Data {
-            Response: Response,
-            ErrorCode: usize,
-            ThrottleSeconds: usize,
-            ErrorStatus: String,
-            Message: String,
-            MessageData: String
+
+#[derive(Debug)]
+struct item_name {
+    name: String
 }
-*/
+
 #[derive(Serialize, Deserialize)]
 struct User { 
             iconPath: String,
@@ -56,9 +55,10 @@ fn main() {
 	let request_type = String::from("205");
 	let platform = '2';
 	let player_name = String::from("cortical_iv");
-	let base_url = String::from("https://www.bungie.net/Platform/Destiny2/");
+	let url = String::from("https://www.bungie.net/Platform/Destiny2/");
+	let item_id = String::from("6917529095138904228");//TODO variable for testing (delete when not needed)
 
-	let mut search_url = base_url.clone();
+	let mut search_url = url.clone();
 	search_url.push_str("SearchDestinyPlayer/");
 	search_url.push(platform);
 	search_url.push('/');
@@ -72,17 +72,20 @@ fn main() {
 		.expect("Failed to send request");
 	let mut buf = String::new();
 	response.read_to_string(&mut buf).expect("Failed to read response");
+	//println!("{}",buf);
 	let user = unwrap_initial_response(buf);
 	let u: User= deserialize_user(user);
-	let equipment = get_gear(&api_key, base_url, platform, u.membershipId, request_type);
-	println!("{}",equipment)
+	let base_url=url.clone();	
+	//let equipment = get_gear(&api_key, base_url, platform, &u.membershipId, &request_type);
+	let item = get_item(item_id);
+	//println!("{}",equipment)
+	//https://www.bungie.net/Platform/2/Profile/4611686018459314819/item/6917529095138904228
 }
 
-fn get_gear(api_key:&String, base_url:String, platform: char, membershipId:String, request_type:String) -> String {
+fn get_gear(api_key:&String, base_url:String, platform: char, membershipId:&String, request_type:&String) -> String {
 	let api_key = get_api_key();
 	let mut url = base_url;
 	//100 is profiles, 200 is characters, 201 is non-equiped items, 205 currently equiped items.
-	let request_type = String::from("205");
 
 	url.push(platform);
 	url.push_str("/Profile/");
@@ -102,42 +105,58 @@ fn get_gear(api_key:&String, base_url:String, platform: char, membershipId:Strin
 	buf
 }
 
-fn unwrap_initial_response(source:String) -> String {
-  let start_index = source.find("[").expect("failed to find open");
-  let end_index = source.find("]").expect("failed to find close");
-  let mut result = &source[(start_index + 1)..end_index];
-  result.to_string()
+fn get_item(item_id2:String) -> String {
+	let mut item_id = String::from("2147998056");
+	let conn : Connection = Connection::open("world_sql_content_b6c7590005d9365b2723f8995f361e3f.content").unwrap();
+	let mut item:String = conn.query_row(
+			r#"SELECT quote(json) 
+			FROM DestinyInventoryItemDefinition 
+			WHERE quote(json) like '%"itemHash":2147998056%'"#,
+			NO_PARAMS,
+			|row| row.get(0),
+			).unwrap();
+
+
+		println!("item: {}", item);
+	item
 }
-  
+
+fn unwrap_initial_response(source:String) -> String {
+	let start_index = source.find("[").expect("failed to find open");
+	let end_index = source.find("]").expect("failed to find close");
+	let mut result = &source[(start_index + 1)..end_index];
+	result.to_string()
+}
+
 fn deserialize_user(ref mut buf:std::string::String) -> User {
-  let u: User = serde_json::from_str(buf).expect("Failed to deserialize user info");
-  u
+	let u: User = serde_json::from_str(buf).expect("Failed to deserialize user info");
+	u
 }
 fn deserialize_item(ref mut buf:std::string::String) -> Item {
-  let i: Item = serde_json::from_str(buf).expect("Failed to deserialize item");
-  i
+	let i: Item = serde_json::from_str(buf).expect("Failed to deserialize item");
+	i
 }
 /*
 //fn get_json(source:String, start:str, end:str) -> String {
 fn get_json(source:String) -> String {
-  let start_index = source.find("2305843009260353573").expect("failed to find open");
-  let end_index = source.find("2305843009260353575").expect("failed to find close");
-  let mut result = &source[(start_index + 31)..(end_index - 4)];
-  result.to_string()
+let start_index = source.find("2305843009260353573").expect("failed to find open");
+let end_index = source.find("2305843009260353575").expect("failed to find close");
+let mut result = &source[(start_index + 31)..(end_index - 4)];
+result.to_string()
 }
-*/
+ */
 fn split_inventory(mut source:String) -> Vec<String> {
-  let mut results = Vec::new();
-    while !source.is_empty() {
-      let start = source.find("{").expect("Failed to find start of item");
-      let end = source.find("}").expect("Failed to find end of item");
-      let item = &source[start..(end+1)];
-      if !item.is_empty() {
-        results.push(item.to_string());
-      }
-      source = source[(end+1)..].to_string();
-    }
-  results
+	let mut results = Vec::new();
+	while !source.is_empty() {
+		let start = source.find("{").expect("Failed to find start of item");
+		let end = source.find("}").expect("Failed to find end of item");
+		let item = &source[start..(end+1)];
+		if !item.is_empty() {
+			results.push(item.to_string());
+		}
+		source = source[(end+1)..].to_string();
+	}
+	results
 }
 
 
